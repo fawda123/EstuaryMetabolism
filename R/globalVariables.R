054d537928ddbffbe7656dfadebd56c820553a98
fawda123 <mbafs2012@gmail.com>
2017-04-14 08:36:29 -0500

    fixing build issues

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,7 +1,7 @@
-globalVariables(c('.', 'atemp', 'binmd', 'binwd', 'bp', 'datetimestamp', 'do_mgl', 'dosat', 'ka', 'kl', 'kv', 'lm', 'na.omit', 'optimize', 'predict', 'rowname', 'sal', 'sig', 'temp', 'wspd'))
+globalVariables(c('.', 'atemp', 'binmd', 'binwd', 'bp', 'datetimestamp', 'do_mgl', 'dosat', 'ka', 'kl', 'kv', 'lm', 'na.omit', 'optimize', 'predict', 'rowname', 'sal', 'sig', 'temp', 'wspd', 'Var1', 'Var2', 'data', 'depth', 'ds', 'dzmix', 'oxySol', 'val', 'var', 'zmix'))
 
-#' @importFrom stats lm na.omit optimize predict
+#' @importFrom stats lm na.omit optimize predict var
 NULL
 
-# #' @importFrom utils data
-# NULL
+#' @importFrom utils data
+NULL
